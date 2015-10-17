ff1c143546bbe6aa15a54b64e347e5d2144d13a9
oxillery <thdepauw@hotmail.com>
2015-10-17 14:05:33 +0200

    RestaurantAtHomeAPI-24 Photo file upload #24 - Added Resto Multi File Upload close #38 changed thumbnail size

--- Rath/Libraries/UploadHandler.php
+++ Rath/Libraries/UploadHandler.php
@@ -97,7 +97,7 @@ class UploadHandler
             'accept_file_types' => '/.+$/i',
             // The php.ini settings upload_max_filesize and post_max_size
             // take precedence over the following max_file_size setting:
-            'max_file_size' => null,
+            'max_file_size' =>  5 * 1024 * 1024,
             'min_file_size' => 1,
             // The maximum number of files for the upload directory:
             'max_number_of_files' => null,
@@ -158,8 +158,8 @@ class UploadHandler
                     // Uncomment the following to force the max
                     // dimensions and e.g. create square thumbnails:
                     //'crop' => true,
-                    'max_width' => 640,
-                    'max_height' => 640
+                    'max_width' => 700,
+                    'max_height' => 700
                 )
             ),
             'print_response' => true //TODO: change to false
